8562e04844abb18976b7955095f57e5ba830609d
Chan Jun Wei <chanjunweimy@gmail.com>
2015-11-16 08:51:34 +0800

    modified logictic regression class to classify emotion
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

--- EmotionRecognizer/src/logisticregression/Sctest.java
+++ EmotionRecognizer/src/logisticregression/Sctest.java
@@ -6,48 +6,37 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Vector;
 
 
 public class Sctest {
-	private static final int NUM_OF_ARGUMENTS = 5;
-	private static final int INDEX_WORD1 = 0;
-	private static final int INDEX_WORD2 = 1;
-	private static final int INDEX_TEST_FILE = 2;
-	private static final int INDEX_MODEL_FILE = 3;
-	private static final int INDEX_ANSWER_FILE = 4;
-
-	private String _word1 = null;
-	private String _word2 = null;
+	private String _emotion = null;
 	private String _testFile = null;
 	private String _modelFile = null;
 	private String _answerFile = null;
 	
-	private HashMap <String, Integer> _allFeature = null;
 	private double[] _weightVectors = null;
 	
-	private static final String CONFUSION_START = ">>";
-	private static final String CONFUSION_END = "<<";
-	private static final int COLLOCATION_LENGTH_BEFORE = 2;
-    private static final int COLLOCATION_LENGTH_AFTER = 2;
+	//classify emotion by feature vectors
+	public Sctest(String emotion, String modelFile) {
+		_emotion = emotion;
+		_modelFile = modelFile;
+		retrieveDataFromModelFile(_modelFile);
+	}
 	
-	private static final String COMMAND_SEPARATOR = ":==:";
-
-	private static String[] STOP_WORDS = null;
-	private static final String STOP_WORDS_FILE = "stopwd.txt";
-	
-	public Sctest(String word1, String word2, String testFile,
+	//classify emotion through file
+	public Sctest(String emotion, String testFile,
 			String modelFile, String answerFile) {
-		readStopWordsFile(STOP_WORDS_FILE);
-		_word1 = word1;
-		_word2 = word2;
+		_emotion = emotion;
 		_testFile = testFile;
 		_modelFile = modelFile;
 		_answerFile = answerFile;
-		_allFeature = new HashMap <String, Integer>();
 		retrieveDataFromModelFile(_modelFile);
 		initializeAnswerFile(_answerFile);
+	}
+	
+	public String getEmotion() {
+		return _emotion;
 	}
 
 	private void initializeAnswerFile(String answerFile) {
@@ -75,15 +64,15 @@ public class Sctest {
 	private boolean processSentence(String line) {
 		line = line.replaceAll("\\s", " ");
 		String[] words = line.split(" ");
-		int[] featureVector = computeFeatureVector(words);
+		double[] featureVector = computeFeatureVector(words);
 		
 		StringBuffer ans = new StringBuffer();
 		ans.append(words[0]);
 		ans.append(" ");
-		if (isWord1(featureVector)) {
-			ans.append(_word1);
+		if (isEmotion(featureVector)) {
+			ans.append(1);
 		} else {
-			ans.append(_word2);
+			ans.append(0);
 		}
 		ans.append("\n");
 		
@@ -104,7 +93,7 @@ public class Sctest {
 		return true;
 	}
 	
-	private boolean isWord1(int[] featureVector) {
+	public boolean isEmotion(double[] featureVector) {
 		if (featureVector.length != _weightVectors.length) {
 			System.out.println("featureVector does not have the same dimention with weight vector");
 			System.exit(-1);
@@ -123,123 +112,54 @@ public class Sctest {
 		return probability >= 0.5;
 	}
     
-	private int[] computeFeatureVector(String[] words) {
+	private double[] computeFeatureVector(String[] words) {
         //words = removeStopWords(words);
-		int[] featureVector = new int[_weightVectors.length];
+		double[] featureVector = new double[_weightVectors.length];
 		Arrays.fill(featureVector, 0);
 		featureVector[0] = 1;
 		
-		int confuseIndexStart = -1, confuseIndexEnd = -1;
-		for (int j = 1; j < words.length; j++) {
-			words[j] = words[j].toLowerCase().trim();
-			String word = words[j];
-			
-			//System.out.println(word);
-
-			if (_allFeature.containsKey(word)) {
-				//System.out.println(word);
-
-				Integer index = _allFeature.get(word);
-				featureVector[index.intValue()] = 1;
-			} else if (CONFUSION_START.equals(word) &&
-					   CONFUSION_END.equals(words[j + 1].trim())) {
-				confuseIndexStart = j;
-				confuseIndexEnd = j + 1;
-
-				j++;
-			} 
-		}
-
-		if (confuseIndexEnd < 0 || confuseIndexStart < 0) {
-			System.out.println("error!");
-            System.exit(-1);
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        sb.insert(0, CONFUSION_START);
-		for (int j = confuseIndexStart - 1; 
-             j > Math.max(confuseIndexStart - 1 - COLLOCATION_LENGTH_BEFORE, 1); j--) {
-			String word = words[j].trim();
-
-			sb.insert(0, " ");
-
-			sb.insert(0, word);
-			String collocationWord = sb.toString().trim();
-			//System.out.println(collocationWord);
-
-			if (_allFeature.containsKey(collocationWord)) {
-				//System.out.println(collocationWord);
-
-				Integer index = _allFeature.get(collocationWord);
-				featureVector[index.intValue()] = 1;
-			}
-		}
-
-        StringBuffer collacationWordBuffer = new StringBuffer();
-        collacationWordBuffer.append(CONFUSION_END);
-		for (int j = confuseIndexEnd + 1; 
-             j < Math.min(confuseIndexEnd + 1 + COLLOCATION_LENGTH_AFTER, words.length - 1); j++) {
-			String word = words[j].trim();
-			
-			collacationWordBuffer.append(" ");
-			collacationWordBuffer.append(word);
-			String collocationWord = collacationWordBuffer.toString().trim();
-			if (_allFeature.containsKey(collocationWord)) {
-				//System.out.println(collocationWord);
-
-				Integer index = _allFeature.get(collocationWord);
-				featureVector[index.intValue()] = 1;
+		for (int i = 0; i < words.length; i++) {
+			String word = words[i];
+			if (isDouble(word)) {
+				featureVector[i] = Double.parseDouble(word);
+			} else {
+				featureVector[i] = 0;
+				System.out.println("got feature token not double!");
 			}
 		}
 
 		return featureVector;
 	}
 
-	private boolean readStopWordsFile(String trainFile) {
-		try {
-			File file = new File(trainFile);
-		    BufferedReader br = new BufferedReader(new FileReader(file));
-		    Vector <String> stopWords = new Vector<String>();
-		    for(String line; (line = br.readLine()) != null; ) {
-		        // process the line.
-		        line = line.trim().toLowerCase();
-		        stopWords.add(line);
-		    }
-		    br.close();
-
-		    STOP_WORDS = new String[stopWords.size()];
-		    for (int i = 0; i < stopWords.size(); i++) {
-		    	STOP_WORDS[i] = stopWords.get(i);
-		    }
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-
-		return true;
-	}
+    private boolean isDouble(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
 	private void retrieveDataFromModelFile(String fileName) {
 		try {
 			File file = new File(fileName);
 		    BufferedReader br = new BufferedReader(new FileReader(file));
-		    int index = 0;
 		    Vector <Double> weights = new Vector <Double>();
 		    for(String line; (line = br.readLine()) != null; ) {
 		        // process the line.
 		        line = line.trim();
-		        String[] lineTokens = line.split(COMMAND_SEPARATOR);
-		        String feature = lineTokens[0].trim();
-
-		        if (_allFeature.containsKey(feature)) {
-		        	System.out.println(feature);
+		        String[] lineTokens = line.split(" ");
+		        for (int i = 0; i < lineTokens.length; i++) {
+		        	double weight;
+		        	if (!isDouble(lineTokens[i])) {
+		        		System.out.println("some weight is not double!");
+		        		weight = 0;
+		        	} else {
+		        		weight = Double.parseDouble(lineTokens[i].trim());
+		        	}
+	        		//System.out.println(weight);
+			        weights.add(weight);
 		        }
-
-		        _allFeature.put(feature, index);
-		        double weight = Double.parseDouble(lineTokens[1].trim());
-		        //System.out.println(weight);
-		        weights.add(weight);
-		        index++;
 		    }
 
 		    _weightVectors = new double[weights.size()];
@@ -248,13 +168,6 @@ public class Sctest {
 				//System.out.println(_weightVectors[i]);
 			}
 			br.close();
-			if (_weightVectors.length != _allFeature.size()) {
-				System.out.println(_weightVectors.length);
-				System.out.println(_allFeature.size());
-				System.out.println("number of features are not same with weight vectors");
-				System.exit(-1);
-			}
-
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(-1);
@@ -263,43 +176,5 @@ public class Sctest {
 		
 		
 		
-	}
-
-	private static Sctest initSctest(String[] args) {
-		String inputFormat = "Input format: java sctest word1 word2 test_file model_file answer_file";
-
-		if (args.length < NUM_OF_ARGUMENTS) {
-			System.out.println("Error: not enough arguments");
-			System.out.println(inputFormat);
-			System.exit(-1);
-		} else if (args.length > NUM_OF_ARGUMENTS) {
-			System.out.println("Error: too many arguments");
-			System.out.println(inputFormat);
-			System.exit(-1);
-		} else {
-			//correct, do ntg
-		}
-
-		String word1 = args[INDEX_WORD1];
-		String word2 = args[INDEX_WORD2];
-		String testFile = args[INDEX_TEST_FILE];
-		String modelFile = args[INDEX_MODEL_FILE];
-		String answerFile = args[INDEX_ANSWER_FILE];
-
-		return new Sctest(word1, word2, testFile, modelFile, answerFile);
-	}
-    
-    public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-
-        Sctest test = initSctest(args);
-        if (!test.startClassifying()) {
-        	System.out.println("not clasifying");
-        	System.exit(-1);
-        }
-        
-        long endTime   = System.currentTimeMillis();
-        long totalTime = endTime - startTime;
-        System.out.println("total time: " + totalTime);
 	}
 }
--- EmotionRecognizer/src/logisticregression/Sctrain.java
+++ EmotionRecognizer/src/logisticregression/Sctrain.java
@@ -6,87 +6,34 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Vector;
-import java.util.regex.Pattern;
 
 public class Sctrain {
-	private static final int INDEX_WORD1 = 0;
-	private static final int INDEX_WORD2 = 1;
-	private static final int INDEX_TRAIN_FILE = 2;
-	private static final int INDEX_MODEL_FILE = 3;
-	private static final int NUM_OF_ARGUMENTS = 4;
-
-	private static String[] STOP_WORDS = null;
-	private static final String STOP_WORDS_FILE = "stopwd.txt";
-
-	private static final String REGEX_PUNCTUATION = "\\p{Punct}";
-
-	private static final String CONFUSION_START = ">>";
-	private static final String CONFUSION_END = "<<";
-	private static final int COLLOCATION_LENGTH_BEFORE = 2;
-    private static final int COLLOCATION_LENGTH_AFTER = 2;
 
 
 	private static final double LEARNING_RATE = 0.05;
 	private static final double GRADIENT_TRESHOLD = 0.0001;
 	private static final double INITIAL_WEIGHT = 0.01;
 
-	private String _word1 = null;
-	private String _word2 = null;
+	private String _emotion = null;
 	private String _trainFile = null;
 	private String _modelFile = null;
 
 	private Vector< Vector<String> > _datasets = null;
-	private int[][] _featureVectors = null;
+	private double[][] _featureVectors = null;
 	private int[] _outcome = null;
 	private double[] _weightVectors = null;
 
-	private HashMap <String, Integer> _surroundingWordsFeature = null;
-	private HashMap <String, Integer> _collocationsFeature = null;
 
-	public Sctrain(String word1, String word2, String trainFile, String modelFile) {
-		readStopWordsFile(STOP_WORDS_FILE);
-		setWord1(word1);
-		setWord2(word2);
+	public Sctrain(String emotion, String trainFile, String modelFile) {
+		setEmotion(emotion);
 		setTrainFile(trainFile);
 		setModelFile(modelFile);
 		_datasets = new Vector< Vector<String> >();
-		_surroundingWordsFeature = new HashMap <String, Integer>();
-		_collocationsFeature = new HashMap <String, Integer>();
 	}
 
-	private boolean readStopWordsFile(String trainFile) {
-		try {
-			File file = new File(trainFile);
-		    BufferedReader br = new BufferedReader(new FileReader(file));
-		    Vector <String> stopWords = new Vector<String>();
-		    for(String line; (line = br.readLine()) != null; ) {
-		        // process the line.
-		        line = line.trim().toLowerCase();
-		        stopWords.add(line);
-		    }
-		    br.close();
-
-		    STOP_WORDS = new String[stopWords.size()];
-		    for (int i = 0; i < stopWords.size(); i++) {
-		    	STOP_WORDS[i] = stopWords.get(i);
-		    }
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-
-		return true;
-	}
-
-	private void setWord1(String word1) {
-		_word1 = word1;
-	}
-
-	private void setWord2(String word2) {
-		_word2 = word2;
+	private void setEmotion(String emotion) {
+		_emotion = emotion;
 	}
 
 	private void setTrainFile(String trainFile) {
@@ -104,13 +51,12 @@ public class Sctrain {
 		    for(String line; (line = br.readLine()) != null; ) {
 		        // process the line.
 		        line = line.replaceAll("\\s", " ");
-		        String[] lineTokens = line.split(" ");
+		        String[] lineTokens = line.split(" ");      
+		        
 		        Vector<String> vector = new Vector<String>();
                 for (int i = 0; i < lineTokens.length; i++) {
                     lineTokens[i] = lineTokens[i].toLowerCase().trim();
-                    if (!lineTokens[i].isEmpty() 
-                    //&& !isStopWord(lineTokens[i])
-                    ) {
+                    if (!lineTokens[i].isEmpty()) {
                     	vector.add(lineTokens[i]);	
                     }
                 }
@@ -130,7 +76,7 @@ public class Sctrain {
 	}
 
 	private void initializeFeatureVector(int numOfDataSets, int numOfFeature) {
-		_featureVectors = new int[numOfDataSets][numOfFeature];
+		_featureVectors = new double[numOfDataSets][numOfFeature];
 		for (int i = 0; i < numOfDataSets; i++) {
             for (int j = 0; j < numOfFeature; j++) {
                 _featureVectors[i][j] = 0;
@@ -144,20 +90,38 @@ public class Sctrain {
 	}
 
 	private boolean retrieveFeaturesFromDataSet() {
-		int index = 1;
-		index = retrieveSurroundingWordsFeature(index);
-		index = retrieveCollocationsFeature(index);
-
-		boolean isSuccess = index >= 0;
-		if (isSuccess) {            
-			initializeWeightVector(index);
-			initializeFeatureVector(_datasets.size(), index);
-			initializeOutcomeVector(_datasets.size());
-		} 
-		return isSuccess;
+		if (_datasets.size() <= 0) {
+			return false;
+		}
+		
+		int numOfDataSets = _datasets.size();
+		int numOfFeatures = _datasets.get(0).size();
+		initializeWeightVector(numOfFeatures);
+		initializeFeatureVector(numOfDataSets, numOfFeatures);
+		initializeOutcomeVector(numOfDataSets);
+		
+		for (int i = 0; i < numOfDataSets; i++) {
+			if (_datasets.get(i).get(0).equals(_emotion)) {
+				_outcome[i] = 1;
+			} else {
+				_outcome[i] = 0;
+			}
+			_featureVectors[i][0] = 1;
+			
+			for (int j = 1; j < numOfFeatures; j++) {
+				String data = _datasets.get(i).get(j);
+				if (isDouble(data)) {
+					_featureVectors[i][j] = Double.parseDouble(data);
+				} else {
+					_featureVectors[i][j] = 0;
+					System.out.println("got feature token not double!");
+				}
+			}
+		}
+		return true;
 	}
 
-    boolean isDouble(String str) {
+    private boolean isDouble(String str) {
         try {
             Double.parseDouble(str);
             return true;
@@ -166,174 +130,7 @@ public class Sctrain {
         }
     }
     
-	private boolean isStopWord(String word) {
-		word = word.trim();
-		for (String stopWord : STOP_WORDS) {
-			if (stopWord.equals(word)) {
-				return true;
-			}	
-		}
-		return false;
-	}
 
-	private int retrieveSurroundingWordsFeature(int index) {
-		if (index < 0) {
-			return index;
-		}
-
-		for (Vector <String> dataset : _datasets) {
-			for (int i = 1; i < dataset.size(); i++) {
-                String word = dataset.get(i);
-				word = word.trim();
-				word = word.toLowerCase();
-				if (word.isEmpty()) {
-					continue;
-				} else if (Pattern.matches(REGEX_PUNCTUATION, word)) {
-					continue;
-				} else if (CONFUSION_START.equals(word)) {
-					i += 2;
-					continue;
-				} else if (CONFUSION_END.equals(word)) {
-					continue;
-				} else if (isStopWord(word)) {
-					continue;
-                } else if (isDouble(word)){
-                    continue;
-                } else if (_surroundingWordsFeature.containsKey(word)) {
-					continue;
-				} else {
-					_surroundingWordsFeature.put(word, index);
-					index++;
-				}
-			}
-		}
-
-		return index;
-	}
-
-	private int retrieveCollocationsFeature(int index) {
-		if (index < 0) {
-			return index;
-		}
-
-		for (Vector <String> dataset : _datasets) {
-			int confussionWordIndex = -1;
-			for (int i = 0; i < dataset.size(); i++) {
-				String token = dataset.get(i).trim();
-				if (CONFUSION_START.equals(token)) {
-					confussionWordIndex = i;
-					break;
-				}
-			}
-
-			if (confussionWordIndex < 0) {
-				//error
-				System.out.println("for dataset " + dataset.get(0).trim() + 
-					" : confussion word\'s index is not found");
-				return confussionWordIndex;
-			}
-
-            StringBuilder sb = new StringBuilder();
-            sb.insert(0, CONFUSION_START);
-			for (int i = confussionWordIndex - 1; 
-                 i > Math.max(confussionWordIndex - 1 - COLLOCATION_LENGTH_BEFORE, 1); i--) {
-				String word = dataset.get(i).trim();
-
-				sb.insert(0, " ");
-                sb.insert(0, word);
-				String collocationWord = sb.toString();
-				if (!_collocationsFeature.containsKey(collocationWord)) {
-					_collocationsFeature.put(collocationWord, index);
-					index ++;
-				}
-			}
-
-			StringBuffer collacationWordBuffer = new StringBuffer();
-			collacationWordBuffer.append(CONFUSION_END);
-			for (int i = confussionWordIndex + 3; 
-                 i < Math.min(confussionWordIndex + 3 + COLLOCATION_LENGTH_AFTER, dataset.size() - 1); i++) {
-				String word = dataset.get(i).trim();
-				
-				collacationWordBuffer.append(" ");
-				collacationWordBuffer.append(word);
-				String collocationWord = collacationWordBuffer.toString();
-				if (!_collocationsFeature.containsKey(collocationWord)) {
-					_collocationsFeature.put(collocationWord, index);
-					index ++;
-				}
-			}
-		}
-		return index;
-	}
-
-	private void computeFeatureVectorsAndOutcome() {
-		for (int i = 0; i < _datasets.size(); i++) {
-			Vector <String> dataset = _datasets.get(i);
-
-			_featureVectors[i][0] = 1;
-			int confuseIndexStart = -1, confuseIndexEnd = -1;
-			for (int j = 0; j < dataset.size(); j++) {
-				String word = dataset.get(j).trim();
-
-				if (_surroundingWordsFeature.containsKey(word)) {
-					Integer index = _surroundingWordsFeature.get(word);
-					_featureVectors[i][index.intValue()] = 1;
-				} else if (CONFUSION_START.equals(word) &&
-						   CONFUSION_END.equals(dataset.get(j + 2).trim())) {
-					confuseIndexStart = j;
-					confuseIndexEnd = j + 2;
-
-					String correctWord = dataset.get(j + 1).trim();
-					if (_word1.equals(correctWord)) {
-						_outcome[i] = 1;
-					} else if (_word2.equals(correctWord)) {
-						_outcome[i] = 0;
-					} else {
-						System.out.println("the guessing word is neither word 1 (" + _word1 +
-							")nor 2 (" + _word2 + ")");
-						System.exit(-1);
-					}
-
-					j += 2;
-				} 
-			}
-            
-            if (confuseIndexEnd < 0 || confuseIndexStart < 0) {
-                System.exit(-1);
-            }
-            
-            StringBuilder sb = new StringBuilder();
-            sb.insert(0, CONFUSION_START);
-			for (int j = confuseIndexStart - 1; 
-                 j >= Math.max(confuseIndexStart - 1 - COLLOCATION_LENGTH_BEFORE, 1); j--) {
-				String word = dataset.get(j).trim();
-				sb.insert(0, " ");
-				sb.insert(0, word);
-				String collocationWord = sb.toString();
-				if (_collocationsFeature.containsKey(collocationWord)) {
-					Integer index = _collocationsFeature.get(collocationWord);
-					_featureVectors[i][index.intValue()] = 1;
-				}
-			}
-
-            StringBuffer collacationWordBuffer = new StringBuffer();
-            collacationWordBuffer.append(CONFUSION_END);
-			for (int j = confuseIndexEnd + 1; 
-                 j <= Math.min(confuseIndexEnd + 1 + COLLOCATION_LENGTH_AFTER, dataset.size() - 1); j++) {
-				String word = dataset.get(j).trim();
-				collacationWordBuffer.append(" ");
-				collacationWordBuffer.append(word);
-				String collocationWord = collacationWordBuffer.toString();
-				if (_collocationsFeature.containsKey(collocationWord)) {
-					Integer index = _collocationsFeature.get(collocationWord);
-					_featureVectors[i][index.intValue()] = 1;
-				} else {
-					//System.out.println(collocationWord);
-				}
-			}
-
-		}
-	}
 
     private double calculateInnerProductOfWeightAndFeature(int indexOfDataSet, double[] weightVector) {
         double innerProductOfWeightAndFeature = 0.0;
@@ -349,16 +146,12 @@ public class Sctrain {
 		 *	xi * (y - z), z = 1/(1+e^(-w x))
 		 */
 
-		int xi = _featureVectors[indexOfDataSet][indexOfFeature];
+		double xi = _featureVectors[indexOfDataSet][indexOfFeature];
 		int y = _outcome[indexOfDataSet];
 
 		double z = Math.pow(Math.E, innerProductOfWeightAndFeature);
 		z += 1;
 		z = 1 / z;
-        
-        if (xi == 1) {
-            //System.out.println(xi * (y - z));
-        }
         
 		return xi * (y - z);
 	}
@@ -439,39 +232,18 @@ public class Sctrain {
 			return false;
 		} 
 
-		computeFeatureVectorsAndOutcome();
-        //printFeatureVectors();  
 		computeStochasticGradientAscent();
-		boolean isSucess = saveFeaturesAndWeights(_surroundingWordsFeature,
-												  _collocationsFeature,
-												  _weightVectors,
+		boolean isSucess = saveFeaturesAndWeights(_weightVectors,
 												  _modelFile);
 		return isSucess;
 	}
 	
-	private boolean saveFeaturesAndWeights(HashMap<String, Integer> surroundingWords, 
-										   HashMap<String, Integer> collocation,
-										   double[] weightVector, String filename) {
+	private boolean saveFeaturesAndWeights(double[] weightVector, String filename) {
 		FileWriter fw;
 		try {
 			fw = new FileWriter(filename);
-			fw.write("interception1111:==:" + weightVector[0] + "\n");
-			for(Map.Entry<String, Integer> entry : surroundingWords.entrySet()) {
-				String key = entry.getKey();
-				Integer value = entry.getValue();
-				
-				fw.write(key + ":==:" + weightVector[value.intValue()] + "\n");
-			}
-			
-			for(Map.Entry<String, Integer> entry : collocation.entrySet()) {
-				String key = entry.getKey();
-				Integer value = entry.getValue();
-
-				fw.write(key + ":==:" + weightVector[value.intValue()] + "\n");
-			}
-		 
-			if (weightVector.length != surroundingWords.size() + collocation.size() + 1) {
-				System.out.println("why?!");
+			for (int i = 0; i < weightVector.length; i++) {
+				fw.write(weightVector[i] + " ");
 			}
 
 			fw.close();
@@ -485,39 +257,4 @@ public class Sctrain {
 		return true;
 	}
 
-	private static Sctrain initSctrain(String[] args) {
-		String inputFormat = "Input format: java sctrain word1 word2 train_file model_file";
-
-		if (args.length < NUM_OF_ARGUMENTS) {
-			System.out.println("Error: not enough arguments");
-			System.out.println(inputFormat);
-			System.exit(-1);
-		} else if (args.length > NUM_OF_ARGUMENTS) {
-			System.out.println("Error: too many arguments");
-			System.out.println(inputFormat);
-			System.exit(-1);
-		} else {
-			//correct, do ntg
-		}
-
-		String word1 = args[INDEX_WORD1];
-		String word2 = args[INDEX_WORD2];
-		String trainFile = args[INDEX_TRAIN_FILE];
-		String modelFile = args[INDEX_MODEL_FILE];
-		return new Sctrain(word1, word2, trainFile, modelFile);
-	}
-
-	public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-        Sctrain train = initSctrain(args);
-		boolean isSuccess = train.startTraining();
-		
-		if (!isSuccess) {
-			System.exit(-1);
-		}
-        
-        long endTime   = System.currentTimeMillis();
-        long totalTime = endTime - startTime;
-        System.out.println("total time: " + totalTime);
-	}
 }
